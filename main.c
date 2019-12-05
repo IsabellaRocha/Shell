71728b3d6863d9ead73340c00764c6d55c6bbb81
clairetempelman <clairept2009@yahoo.com>
2019-12-05 15:24:11 -0500

    add

--- main.c
+++ main.c
@@ -7,6 +7,10 @@ int main() {
         printf("$ ");
         char** args;
         fgets(line, 100, stdin);
+        char * checker;
+        if ((checker = strchr(line, '\n')) != NULL) {
+          *checker = '\0';
+        }
         args = parse_args(line, 10);
         if(strcmp(args[0], "exit") == 0){
             exit(0);
