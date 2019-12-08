0ed3cfe87da7cbf4ac51ac2c27f778dc57dd5ae6
Isabella Rocha <irocha00@stuy.edu>
2019-12-08 15:51:56 -0500

    Typos in main

--- main.c
+++ main.c
@@ -5,14 +5,16 @@ int main() {
     while (strcmp(line, "exit") != 0) {
         printf("$ ");
         char** args;
-        fgets(line, 100, stdin);
+        fgets(line, 50, stdin);
         char * checker;
-        line[strlen(line) - 1] = 0;
+        if ((checker = strchr(line, '\n')) != NULL) {
+          *checker = '\0';
+        }
         args = parse_args(line, ";"); //Take into account multple arguments at once
         int idx = 0;
         while (strcmp(args[idx], "exit") != 0 && args[idx] != NULL) {
             char **args2 = parse_args(args[idx], " ");
-            execute(args);
+            execute(args2);
             idx++;
         }
     }
