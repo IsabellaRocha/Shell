4a212dc7e1e2fa255a5313d7c36c3888a684e276
Isabella Rocha <irocha00@stuy.edu>
2019-12-11 23:09:52 -0500

    minor changes

--- shell.c
+++ shell.c
@@ -64,18 +64,21 @@ void redirect(char ** args) {
       for (; args[c] != NULL; c++){
         if (strcmp(args[c], ">") == 0){
             fd = open(args[c + 1], O_CREAT|O_WRONLY, 0744);  //Granting read and write permissions
+            args[c] = "\0";
             backup = dup(STDOUT_FILENO);
             dup2(fd, STDOUT_FILENO);
             close(fd);
         }
         if (strcmp(args[c], "<") == 0){
             fd = open(args[c + 1], O_RDONLY, 0);
+            args[c] = "\0";
             backup = dup(STDIN_FILENO);
             dup2(fd, STDIN_FILENO);
             close(fd);
         }
         if (strcmp(args[c], ">>") == 0){
             fd = open(args[c + 1], O_CREAT|O_APPEND, 0744);
+            args[c] = "\0";
             backup = dup(0);
             dup2(fd, 0);
             close(fd);
