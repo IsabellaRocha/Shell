9ebec940f6b0ac38aae853927eea3400657ad00f
Isabella Rocha <irocha00@stuy.edu>
2019-12-15 15:01:42 -0500

    New approach to redirect

--- shell.c
+++ shell.c
@@ -2,7 +2,7 @@
 
 /*      char** parse_args()
 Inputs: char * line -> The input string which we are parsing over
-        char * delimiter -> The string of characters we are looking for in line
+char * delimiter -> The string of characters we are looking for in line
 
 Every time a delimiter is found in line, a new token is created and added to the array of arguments
 
@@ -33,39 +33,39 @@ First removes all leading spaces, then loops through line and removes any extra 
 Returns a new string with all the extra spaces removed
 */
 char * removeSpace(char * line) {
-  char arg[50];
-  char newLine[50];
-  strcpy(arg, line);
-  int idx = 0;
-  int cur = 0;
-  bool space = false;
-  while(idx < strlen(line) && arg[idx] == ' ') { //Get rid of leading spaces
-      idx++;
-  }
-  while(idx < strlen(line)) {
-      if(arg[idx] != ' ') {
-          space = false;
-          newLine[cur] = arg[idx];
-          cur++;
-          idx++;
-      }
-      if(arg[idx] == ' ' && space){
-          idx++;
-      }
-      if(arg[idx] == ' ' && !space) {
-          space = true;
-          newLine[cur] = arg[idx];
-          idx++;
-          cur++;
-      }
-  }
-  if(cur > 0 && newLine[cur - 1] == ' ') { //In case there's a space after the last charcter
-      newLine[cur - 1] = '\0';
-  }
-  newLine[cur] = '\0'; //Null to terminate execvp
-  char * parse = malloc(50 * sizeof(char));
-  strcpy(parse, newLine);
-  return parse;
+    char arg[50];
+    char newLine[50];
+    strcpy(arg, line);
+    int idx = 0;
+    int cur = 0;
+    bool space = false;
+    while(idx < strlen(line) && arg[idx] == ' ') { //Get rid of leading spaces
+        idx++;
+    }
+    while(idx < strlen(line)) {
+        if(arg[idx] != ' ') {
+            space = false;
+            newLine[cur] = arg[idx];
+            cur++;
+            idx++;
+        }
+        if(arg[idx] == ' ' && space){
+            idx++;
+        }
+        if(arg[idx] == ' ' && !space) {
+            space = true;
+            newLine[cur] = arg[idx];
+            idx++;
+            cur++;
+        }
+    }
+    if(cur > 0 && newLine[cur - 1] == ' ') { //In case there's a space after the last charcter
+    newLine[cur - 1] = '\0';
+}
+newLine[cur] = '\0'; //Null to terminate execvp
+char * parse = malloc(50 * sizeof(char));
+strcpy(parse, newLine);
+return parse;
 }
 
 /*      void cd()
@@ -91,37 +91,56 @@ Does not return anything, only modifies the file table
 */
 void redirect(char ** args) {
     int fd;
-    int backup;
-      int c = 0;
-      for (; args[c] != NULL; c++){
+    int c = 0;
+    char input[50];
+    bool in = false;
+    bool out = false;
+    bool out2 = false;
+    int backup = 0;
+    for (; args[c] != NULL; c++){
         if (strcmp(args[c], ">") == 0){
-            fd = open(args[c + 1], O_CREAT|O_WRONLY, 0744);  //Granting read and write permissions
-            if (fd < 0){
-              printf("errno %d error: %s\n", errno, strerror(errno));
-            }
-            backup = dup(STDOUT_FILENO);
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
+            args[c] = NULL;
+            strcpy(input, args[c + 1]);
+            out = true;
         }
         if (strcmp(args[c], "<") == 0){
-            fd = open(args[c + 1], O_RDONLY, 0444);
+            args[c] = NULL;
+            strcpy(input, args[c + 1]);
+            in = true;
+        }
+        if (strcmp(args[c], ">>") == 0){
+            args[c] = NULL;
+            strcpy(input, args[c + 1]);
+            out2 = true;
+        }
+        if (in) {
+            fd = open(input, O_RDONLY, 0444);
             if (fd < 0){
-              printf("errno %d error: %s\n", errno, strerror(errno));
+                printf("errno %d error: %s\n", errno, strerror(errno));
             }
             backup = dup(STDIN_FILENO);
             dup2(fd, STDIN_FILENO);
             close(fd);
         }
-        if (strcmp(args[c], ">>") == 0){
-            fd = open(args[c + 1], O_APPEND, 0744);
+        if (out) {
+            fd = open(input, O_CREAT|O_WRONLY, 0744);
             if (fd < 0){
-              printf("errno %d error: %s\n", errno, strerror(errno));
+                printf("errno %d error: %s\n", errno, strerror(errno));
+            }
+            backup = dup(STDIN_FILENO);
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        }
+        if (out2) {
+            fd = open(input, O_APPEND, 0744);
+            if (fd < 0){
+                printf("errno %d error: %s\n", errno, strerror(errno));
             }
             backup = dup(0);
             dup2(fd, 0);
             close(fd);
         }
-      }
+    }
 }
 
 /*      void pip()
@@ -145,7 +164,7 @@ Inputs: char** args -> Array of arguments
 
 Checks if cd appears to call cd, then forks to a child process and calls redirect in case redirection is needed, and finally uses execvp to run the arguments in args
 Does not return anything, simply executes the commands
-*/ 
+*/
 void execute(char** args){
     int status;
     if(strcmp(args[0], "cd") == 0) {
@@ -154,7 +173,7 @@ void execute(char** args){
 
     else {
         if(fork() == 0) {
-        redirect(args);
+            redirect(args);
             execvp(args[0], args);
             if(errno != 0) {
                 printf("Error: %s\n", strerror(errno));
